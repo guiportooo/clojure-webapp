9a205a0769851a89410d659dc50d8adfae499476
Guilherme Porto <guiportooo@gmail.com>
2020-05-19 19:08:03 -0300

    Configure ring plugin

--- src/webapp/core.clj
+++ src/webapp/core.clj
@@ -4,3 +4,13 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn example-handler
+  [request]
+  {:body "Hello Clojure with hot reloading!"})
+
+(defn on-init []
+  (println "Initializing sample webapp!"))
+
+(defn on-destroy []
+  (println "Destroying sample webapp!"))
